9ff05e973aa7581bf6719e3a075c33af4ad82ab4
Christopher Weigle <charlespoetwolf@gmail.com>
2013-12-14 16:06:57 -0500

    Commented

--- singlelinehello.d
+++ singlelinehello.d
@@ -1,1 +1,1 @@
-import std.stdio; void main() { writeln("Hello, World!"); }
+import std.stdio; void main() { writeln("Hello, World!"); } //all this is on a single line and will still return "Hello, World!" when compiled
