421231d6de2f8b019bdd271a5416e559792a384e
avarisclari <weiglec.envisagegame@gmail.com>
2013-12-14 21:58:15 -0500

    Added Lua and Ruby versions
    Signed-off-by: avarisclari <weiglec.envisagegame@gmail.com>

---
+++ Lua-hello-world.lua
@@ -0,0 +1,1 @@
+print("Hello, World!") -- Lua version of Hello World. 
---
+++ Ruby-hello-world.rb
@@ -0,0 +1,1 @@
+puts 'Hello, World!' #Ruby single line that will output hello world (pretty sure this is the smallest one possible)
